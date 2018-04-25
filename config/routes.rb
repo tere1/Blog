a51eeb8c2f97841c068f408f08ff2cc4369f5c42
teresa <mmariateresa4@gmail.com>
2018-04-25 09:44:00 -0500

    First Commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :posts do
+    resources :comments
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'posts#index', as: 'home'
+  get 'about' => 'pages#about', as: 'about'
+
+end
